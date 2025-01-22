7610e8fa82e8f033630f96b8416a57af6b07aac3
Syfain <safenrafet6@gmail.com>
2025-01-22 23:48:20 +0200

    Enhance Hangman game with new features and styling improvements

--- Note.md
+++ Note.md
@@ -3,3 +3,9 @@ A simple hangman game creating with javascript
 ### Features
 - Random word selection
 - more categories
+- if answer not correct answer the stand change color to read color
+- add number and word to guess
+- add timer
+- add score
+- add sound
+
---
+++ master.js
@@ -0,0 +1,14 @@
+
+const letters = "abcdefghijklmnopqrstuvwxyz";
+let lettersArray = Array.from(letters);
+
+// select letters container
+
+let lettersContainer = document.querySelector(".letters");
+lettersArray.forEach(letter => {
+  let span = document.createElement("span");
+  let theLetter = document.createTextNode(letter);
+  span.appendChild(theLetter);
+  span.className = "letter-box";
+  lettersContainer.appendChild(span);
+})
